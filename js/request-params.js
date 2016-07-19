0e6d6dd5ea6fd2bd70b3e3cca408ea5599fe58bb
mattietea <mattcthomas@me.com>
2016-07-19 14:29:17 -0400

    Add request params to join now, login and thank you pages

--- js/request-params.js
+++ js/request-params.js
@@ -1,3 +1,55 @@
+$( document ).ready(function() {
+	$(window).load(function() {
+		var invalid = getParam('invalid');
+		var error = getParam('error');
+		var name = getParam('invalidName');
+		var email = getParam('invalidEmail');
+		var phone = getParam('invalidPhone');
+		var zip = getParam('invalidZip');
+		var contact = getParam('contact');
+		var register = getParam('register');
+		var subscribe = getParam('subscribe');
+
+console.log("hello");
+
+		if (invalid == 'true') {
+			$("#general-error").addClass('show');
+		}
+
+		if (error == 'true') {
+			$("#bad-error").addClass('show');
+		}
+
+		if (name == 'true') {
+			$("#name-error").addClass('show');
+		}
+
+		if (email == 'true') {
+			$("#email-error").addClass('show');
+		}
+
+		if (phone == 'true') {
+			$("#phone-error").addClass('show')
+		}
+
+		if (zip == 'true') {
+			$("#zip-error").addClass('show')
+		}
+
+		if (contact == 'true') {
+			$("#contact-thank-you").addClass('show');
+		}
+
+		if (subscribe == 'true') {
+			$("#contact-thank-you").addClass('show');
+		}
+
+		if (register == 'true') {
+			$("#register-thank-you").addClass('show');
+		}
+	});
+});
+
 function getParam(param) {
 	var vars = {};
 	window.location.href.replace(location.hash, '').replace(
@@ -11,11 +63,4 @@ function getParam(param) {
 		return vars[param] ? vars[param] : null;
 	}
 	return vars;
-}
-
-
-var invalid = getParam('invalid');
-
-if (invalid == 'true') {
-	$("#invalid").toggleClass("hide");
 }
